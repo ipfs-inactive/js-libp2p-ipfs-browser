93a8d2eef57abd1199c7be9ccd9e1d4493310749
David Dias <daviddias.p@gmail.com>
2017-01-29 08:40:48 +0000

    feat: enable bootstrap to be an option

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const WS = require('libp2p-websockets')
 const WebRTCStar = require('libp2p-webrtc-star')
 const spdy = require('libp2p-spdy')
 const secio = require('libp2p-secio')
+const Railing = require('libp2p-railing')
 const libp2p = require('libp2p')
 
 class Node extends libp2p {
   constructor (peerInfo, peerBook, options) {
+    options = options || {}
     const webRTCStar = new WebRTCStar()
 
     const modules = {
@@ -27,6 +29,12 @@ class Node extends libp2p {
         webRTCStar.discovery
       ]
     }
+
+    if (options.bootstrap && process.env.IPFS_BOOTSTRAP) {
+      const r = new Railing(options.bootstrap)
+      modules.discovery.push(r)
+    }
+
     super(modules, peerInfo, peerBook, options)
   }
 }
