d4e6356ae32a1f8f0e3d316f77cc92ee60070aaf
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-24 18:07:51 +0100

    feat: add multiplex support by default
    This also allows to set `options.muxer` to specify the used muxers
    
    BREAKING CHANGE: Prefer multiplex over spdy, as it works better with go-ipfs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@
 const WS = require('libp2p-websockets')
 const WebRTCStar = require('libp2p-webrtc-star')
 const spdy = require('libp2p-spdy')
+const multiplex = require('libp2p-multiplex')
 const secio = require('libp2p-secio')
 const Railing = require('libp2p-railing')
 const libp2p = require('libp2p')
+
+function mapMuxers (list) {
+  return list.map((pref) => {
+    if (typeof pref !== 'string') {
+      return pref
+    }
+    switch (pref.trim().toLowerCase()) {
+      case 'spdy':
+        return spdy
+      case 'multiplex':
+        return multiplex
+      default:
+        throw new Error(pref + ' muxer not available')
+    }
+  })
+}
+
+function getMuxers (options) {
+  if (options) {
+    return mapMuxers(options)
+  } else {
+    return [multiplex, spdy]
+  }
+}
 
 class Node extends libp2p {
   constructor (peerInfo, peerBook, options) {
@@ -18,9 +43,7 @@ class Node extends libp2p {
         webRTCStar
       ],
       connection: {
-        muxer: [
-          spdy
-        ],
+        muxer: getMuxers(options.muxer),
         crypto: [
           secio
         ]
